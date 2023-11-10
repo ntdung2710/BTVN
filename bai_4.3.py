04a8c3cca6455629b0c24b128c94f68fdebef206
khoa học dữ liệu 17A2 <ntdung.dhkl17a2hn@sv.uneti.edu.vn>
2023-11-10 23:59:06 +0700

    Bài tập chương 4

---
+++ bai_4.3.py
@@ -0,0 +1,6 @@
+#4.3
+m=int (input("Nhập vào giá trị m:"))
+n=int(input("Nhập vào giá trị n :"))
+for i in range (1,n):
+    if i%m==0:
+        print (i)
---
+++ bai_4.5.py
@@ -0,0 +1,8 @@
+#4.5
+a=int(input("Nhập vào số nguyên a :"))
+b=int(input("Nhập vào số nguyên b :"))
+x,y=a,b
+while x != y :
+     if x > y : x = x-y
+     else : y = y - x
+print ("Bội chung nhỏ nhất của 2 số",a,"va",b,"la :",a*b//x)
